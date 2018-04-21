d909341f2b37a40a9bf61015e54eab2bf500375f
huangweiqi <askviky2010@gmail.com>
2018-04-21 23:44:39 +0800

    重构代码

--- app/src/main/java/com/abt/badge/MainActivity.java
+++ app/src/main/java/com/abt/badge/MainActivity.java
@@ -1,12 +1,14 @@
 package com.abt.badge;
 
 import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
-import android.widget.LinearLayout;
+import android.view.View;
+import android.widget.FrameLayout;
 
 import com.abt.badgeview.BadgeFactory;
+import com.abt.badgeview.BadgeView;
 import com.orhanobut.logger.Logger;
 
 /**
@@ -14,30 +16,45 @@ import com.orhanobut.logger.Logger;
  */
 public class MainActivity extends AppCompatActivity {
 
+    private FrameLayout mLayout = null;
+    private BadgeView mBadgeView = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+    }
 
-        LinearLayout view = (LinearLayout) findViewById(R.id.img_wrapper);
-
-        BadgeFactory.create(this)
+    private void initViews() {
+        mLayout = findViewById(R.id.badge_layout);
+        mBadgeView = BadgeFactory.create(this)
                 .setTextColor(Color.WHITE)
-                .setWidthAndHeight(50,50)
+                .setWidthAndHeight(50, 50)
                 .setBadgeBackground(Color.RED)
                 .setTextSize(22)
-                .setBadgeGravity(Gravity.RIGHT|Gravity.TOP)
+                .setBadgeGravity(Gravity.RIGHT | Gravity.TOP)
                 .setBadgeCount(1)
                 .setShape(com.abt.badgeview.BadgeView.SHAPE_CIRCLE)
-                .setSpace(0,0)
-                .bind(view);
+                .setSpace(0, 0)
+                .bind(mLayout);
     }
 
     @Override
-    protected void onPause() {
-        super.onPause();
+    protected void onResume() {
+        super.onResume();
+        Logger.d("onResume");
 
-        BadgeFactory.create(this).unbind();
-        Logger.d("hello");
+        if (null != mBadgeView) {
+            mBadgeView.setVisibility(View.VISIBLE);
+        }
     }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Logger.d("onStop");
+        BadgeFactory.create(this).unbind();
+    }
+
 }
